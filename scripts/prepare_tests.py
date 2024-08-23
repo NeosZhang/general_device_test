2bb55055c6dab39c6a5f388c6b4972d37d7a6c7b
zhangqiu <zhangqiu1994@outlook.com>
2024-08-23 16:12:51 +0800

    init

---
+++ scripts/prepare_tests.py
@@ -0,0 +1,205 @@
+import shutil
+import os
+import argparse
+import subprocess
+
+from process_test_utils import modify_src_code
+
+
+def sparse_checkout(repo_url, destination, paths):
+    subprocess.run(['git', 'clone', '--no-checkout', repo_url, destination], check=True)
+    os.chdir(destination)
+    subprocess.run(['git', 'sparse-checkout', 'init', '--cone'], check=True)
+    subprocess.run(['git', 'sparse-checkout', 'set'] + paths, check=True)
+    subprocess.run(['git', 'checkout'], check=True)
+    print("Sparse checkout 完成!")
+
+
+# 通过传参设置device_code
+parser = argparse.ArgumentParser()
+parser.add_argument("device_code", type=str, help="the supported device_code are 'cuda', 'npu', 'camb', 'muxi'.")
+args = parser.parse_args()
+device_code = args.device_code
+
+device_torch_path = "./device_torch/"
+origin_torch_path = "./origin_torch/"
+
+sparse_checkout("https://github.com/pytorch/pytorch.git", origin_torch_path, ["test"])
+
+if device_code == "npu":
+    sparse_checkout("https://gitee.com/ascend/pytorch.git", device_torch_path, ["test"])
+
+
+device_test_path = device_torch_path + "test"
+torch_test_path = origin_torch_path + "test"
+
+# torch/test中的某些测例对于国产设备是不必要的，或者对应的测试脚本不适用于国产设备
+# 所以使用该脚本进行过滤，对过滤后的文件进行处理
+unnecessary_tests = [
+    "_nvfuser",
+    "_test_bazel.py",
+    "ao",
+    "ao/sparsity",
+    "backends",
+    "benchmark_utils",
+    "cpp",
+    "cpp_api_parity",
+    "cpp_extensions",
+    "custom_backend",
+    "custom_operator",
+    "distributed",
+    "distributions",
+    "dynamo",
+    "edge",
+    "error_messages",
+    "export",
+    "forward_backward_compatibility",
+    "functorch",
+    "fx",
+    "inductor",
+    "jit",
+    "jit_hooks",
+    "lazy",
+    "mobile",
+    "onnx",
+    "onnx_caffe2",
+    "package",
+    "profiler",
+    "quantization",
+    "scripts",
+    "test_img",
+    "torch_np",
+    "typing",
+    "test_ao_sparsity.py",
+    "test_autograd_fallback.py",
+    "test_bundled_images.py",
+    "test_bundled_inputs.py",
+    "test_comparison_utils.py",
+    "test_compile_benchmark_util.py",
+    "test_content_store.py",
+    "test_cpp_extensions_aot.py",
+    "test_cpp_extensions_jit.py",
+    "test_cpp_extensions_open_device_registration.py",
+    "test_cuda_expandable_segments.py",
+    "test_cuda_multigpu.py",
+    "test_cuda_nvml_based_avail.py",
+    "test_cuda_primary_ctx.py",
+    "test_cuda_sanitizer.py",
+    "test_cuda_trace.py",
+    "test_cuda.py",
+    "test_custom_ops.py",
+    "test_dataloader.py",
+    "test_decomp.py",
+    "test_deploy.py",
+    "test_determination.py",
+    "test_dispatch.py",
+    "test_dlpack.py",
+    "test_expanded_weights.py",
+    "test_foreach.py",
+    "test_function_schema.py",
+    "test_functional_autograd_benchmark.py",
+    "test_functional_optim.py",
+    "test_functionalization_of_rng_ops.py",
+    "test_functionalization.py",
+    "test_futures.py",
+    "test_fx_experimental.py",
+    "test_fx_passes.py",
+    "test_fx_reinplace_pass.py",
+    "test_fx.py",
+    "test_hub.py",
+    "test_itt.py",
+    "test_jit_autocast.py",
+    "test_jit_cuda_fuser.py",
+    "test_jit_disabled.py",
+    "test_jit_fuser_legacy.py",
+    "test_jit_fuser_te.py",
+    "test_jit_fuser.py",
+    "test_jit_legacy.py",
+    "test_jit_llga_fuser.py",
+    "test_jit_profiling.py",
+    "test_jit_simple.py",
+    "test_jit_string.py",
+    "test_jit.py",
+    "test_jiterator.py",
+    "test_kernel_launch_checks.py",
+    "test_license.py",
+    "test_linalg.py",
+    "test_logging.py",
+    "test_masked.py",
+    "test_maskedtensor.py",
+    "test_matmul_cuda.py",
+    "test_meta.py",
+    "test_metal.py",
+    "test_mkl_verbose.py",
+    "test_mkldnn_fusion.py",
+    "test_mkldnn_verbose.py",
+    "test_mkldnn.py",
+    "test_mobile_optimizer.py",
+    "test_model_dump.py",
+    "test_module_init.py",
+    "test_modules.py",
+    "test_monitor.py",
+    "test_mps.py",
+    "test_multiprocessing_spawn.py",
+    "test_multiprocessing.py",
+    "test_namedtuple_return_api.py",
+    "test_native_functions.py",
+    "test_native_mha.py",
+    "test_nestedtensor.py",
+    "test_nnapi.py",
+    "test_numba_integration.py",
+    "test_numpy_interop.py",
+    "test_nvfuser_frontend.py",
+    "test_openmp.py",
+    "test_ops_fwd_gradients.py",
+    "test_ops_gradients.py",
+    "test_ops_jit.py",
+    "test_ops.py",
+    "test_out_dtype_op.py",
+    "test_overrides.py",
+    "test_package.py",
+    "test_per_overload_api.py",
+    "test_pruning_op.py",
+    "test_public_bindings.py",
+    "test_python_dispatch.py",
+    "test_pytree.py",
+    "test_quantization.py",
+    "test_segment_reductions.py",
+    "test_set_default_mobile_cpu_allocator.py",
+    "test_show_pickle.py",
+    "test_sparse_semi_structured.py",
+    "test_sparse.py",
+    "test_static_runtime.py",
+    "test_subclass.py",
+    "test_sympy_utils.py",
+    "test_tensor_creation_ops.py",
+    "test_tensorexpr_pybind.py",
+    "test_throughput_benchmark.py",
+    "test_torch.py",
+    "test_transformers.py",
+    "test_type_hints.py",
+    "test_type_info.py",
+    "test_type_promotion.py",
+    "test_typing.py",
+    "test_unary_ufuncs.py",
+    "test_utils.py",
+    "test_view_ops.py",
+    "test_vulkan.py",
+    "test_weak.py",
+    "test_xnnpack_integration.py" 
+]
+
+if device_code == "npu":
+# 1. 从device_test_path拷贝测试数据到当前目录
+    shutil.copytree(device_test_path, "../modified_tests/", dirs_exist_ok=True)
+
+# 2. 从torch_test_path拷贝测试数据到当前目录，如果对应的test在当前目录已存在或在unnecessary_tests中，则跳过
+def ignore_tests(dir, contents):
+    ignore_list =  [name for name in contents if (name in unnecessary_tests or name in os.listdir(device_test_path) or ("." in name and "test_" not in name))]
+    return ignore_list
+shutil.copytree(torch_test_path, "../to_process_tests/", ignore=ignore_tests, dirs_exist_ok=True)
+
+# 3. 对to_process_tests下的文件进行符号替换处理
+os.chdir("../to_process_tests")
+for item in os.listdir(os.getcwd()):
+    modify_src_code(item, device_code)
---
+++ scripts/process_test_utils.py
@@ -0,0 +1,115 @@
+import os
+import re
+import re
+
+def split_script(input_file):
+    # 用于匹配 import 和 from ... import 语句（包括多行）
+    import_pattern = re.compile(r'^\s*(import\s+\w+|from\s+\w+(\.\w+)*\s+import\s+)')
+    continuation_pattern = re.compile(r'^\s*[\(\,]')
+
+    # 读取原始脚本
+    with open(input_file, 'r') as f:
+        lines = f.readlines()
+        
+    search_import_lines = []
+    for line in lines:
+        if "=" in line:
+            break
+        search_import_lines.append(line)
+
+    split_line = 0
+    is_import = False
+    for i in range(len(search_import_lines)):
+        if search_import_lines[i] == "":
+            continue
+        # 检查是否是 import 语句的开始
+        if import_pattern.match(search_import_lines[i]):
+            is_import = True  # 标记为 import 语句
+            split_line = i
+        # 检查是否是多行 import 语句的继续部分
+        elif is_import or (continuation_pattern.match(search_import_lines[i])):
+            # 检查是否结束了 import 语句
+            if ')' in search_import_lines[i]:
+                is_import = False
+                split_line = i
+        else:
+            is_import = False  # 结束 import 语句标记
+            
+
+    # 将结果返回为字符串
+    imports_text = ''.join(lines[:split_line+1])
+    functions_text = ''.join(lines[split_line+1:])
+    return imports_text, functions_text
+
+def device_patch_for_test(device_code: str):
+    import_torch = ""
+    test_code_map = {
+        "onlyCUDAAndPRIVATEUSE1": "onlyCUDAAndPRIVATEUSE1",
+        "TEST_MULTIGPU": "TEST_MULTIGPU",
+        "TEST_CUDA_GRAPH": "TEST_CUDA_GRAPH",
+        "TEST_CUDA": "TEST_CUDA",
+        "TEST_CUDNN": "TEST_CUDNN",
+        "torch.cuda": "torch.cuda",
+        "onlyCUDA": "onlyCUDA",
+        "RUN_CUDA": "RUN_CUDA",
+        "dtypesIfCUDA": "dtypesIfCUDA",
+        ".cuda(":".cuda(",
+        '"cuda"': '"cuda"',
+        "'cuda'": "'cuda'",
+        "cuda:":"cuda:"
+    }
+    if device_code == "cuda":
+        pass
+    elif device_code == "npu":
+        import_torch = "import torch_npu\nimport torch_npu.testing\nfrom torch.testing._internal.common_utils import TEST_PRIVATEUSE1\n\
+from torch.testing._internal.common_device_type import onlyPRIVATEUSE1, dtypesIfPRIVATEUSE1\n\
+TEST_MULTINPU = TEST_PRIVATEUSE1 and torch_npu.npu.device_count() >= 2\n\
+RUN_PRIVATEUSE1_MULTI_GPU = TEST_MULTINPU\n\
+TEST_PRIVATEUSE1_VERSION = 5130\n\
+RUN_PRIVATEUSE1 = True\n\
+TEST_NPU = torch_npu.npu.is_available()\n\
+TEST_BFLOAT16 = TEST_NPU and torch_npu.npu.is_bf16_supported()\n\
+RUN_PRIVATEUSE1_HALF = RUN_PRIVATEUSE1\n"
+
+        test_code_map["onlyCUDAAndPRIVATEUSE1"] = "onlyPRIVATEUSE1"
+        test_code_map["TEST_MULTIGPU"] = "TEST_MULTINPU"
+        test_code_map["TEST_CUDA_GRAPH"] = "TEST_PRIVATEUSE1"
+        test_code_map["TEST_CUDA"] = "TEST_PRIVATEUSE1"
+        test_code_map["TEST_CUDNN"] = "TEST_PRIVATEUSE1"
+        test_code_map["torch.cuda"] = "torch_npu.npu"
+        test_code_map["onlyCUDA"] = "onlyPRIVATEUSE1"
+        test_code_map["RUN_CUDA"] = "RUN_PRIVATEUSE1"
+        test_code_map["dtypesIfCUDA"] = "dtypesIfPRIVATEUSE1"
+        test_code_map[".cuda("] = ".npu("
+        test_code_map['"cuda"'] = '"npu"'
+        test_code_map["'cuda'"] = "'npu'"
+        test_code_map["cuda:"] = "npu:"
+    else:
+        raise ValueError("invald device code! The legal device code are: cuda, npu.")
+
+    return import_torch, test_code_map
+
+def replace_in_text(text, mapping):
+    # 将映射关系字典的键转换为正则表达式可以接受的格式，确保能正确处理特殊字符
+    pattern = re.compile("|".join(re.escape(key) for key in mapping.keys()))
+    
+    # 使用正则表达式替换文本中的键
+    return pattern.sub(lambda x: mapping[x.group()], text)
+
+def modify_src_code(src: str, device_code: str):
+    code_patch = device_patch_for_test(device_code)
+    imports_text, functions_text = split_script(src)
+    
+    imports_text = code_patch[0] + imports_text
+    functions_text = replace_in_text(functions_text, code_patch[1])
+
+    with open(src, "w", encoding="utf-8") as file:
+        file.write(imports_text + functions_text)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", type=str, help="original pytorch test file")
+    args = parser.parse_args()
+    modify_src_code(args.filename, "npu")
+
