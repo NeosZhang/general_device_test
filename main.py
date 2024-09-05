d9c2012c1c7d332993e3dfc6d365aec44c7113df
NeosZhang <zhangqiu1994@outlook.com>
2024-09-05 07:30:37 +0000

    reformat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import shutil
 import os
-import argparse
+import torch
 
 from utils.process_test import process_src_code
 from utils.utils import sparse_checkout
@@ -9,24 +9,22 @@ from utils.unnecessary_tests import unnecessary_tests
 MAIN_DIR = os.path.dirname(os.path.abspath(__file__))
 ORIGIN_TORCH_PATH = MAIN_DIR + "/origin_torch/"
 TORCH_TEST_PATH = ORIGIN_TORCH_PATH + "test"
-SHOW_SKIPPED_TESTS = os.getenv("DISPLAY_SKIPPED_TESTS", False)
+TORCH_URL = "https://github.com/pytorch/pytorch.git"
+
+
+torch_tag = "v" + torch.__version__.split("+")[0]
 
 if not os.path.exists(ORIGIN_TORCH_PATH):
-    sparse_checkout(
-        "https://github.com/pytorch/pytorch.git", ORIGIN_TORCH_PATH, ["test"], "v2.1.0"
-    )
+    sparse_checkout(TORCH_URL, ORIGIN_TORCH_PATH, ["test"], torch_tag)
 
 
 # 从torch_test_path拷贝测试脚本，如果对应的脚本在unnecessary_tests列表中，则跳过
-def ignore_tests(dir, contents):
-    ignore_list = [name for name in contents if (name in unnecessary_tests)]
-    return ignore_list
-
-
 shutil.copytree(
     TORCH_TEST_PATH,
     MAIN_DIR + "/processed_tests/",
-    ignore=ignore_tests,
+    ignore=lambda dir, contents: [
+        name for name in contents if name in unnecessary_tests
+    ],
     dirs_exist_ok=True,
 )
 
